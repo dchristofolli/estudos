4ceb3de7bdd66f75985d5849a26458284f323d5d
dchristofolli <danielchristofolli@gmail.com>
2018-11-16 16:42:16 -0200

    Classe Pessoa criada

--- java_basico/Study_List_Stream/src/com/github/dchristofolli/ListStream/ListStream.java
+++ java_basico/Study_List_Stream/src/com/github/dchristofolli/ListStream/ListStream.java
@@ -2,10 +2,22 @@ package com.github.dchristofolli.ListStream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class ListStream {
+    static List<Pessoa> listaPessoas = Arrays.asList(new Pessoa("Daniel", 35),
+            new Pessoa("Daiane", 35),
+            new Pessoa("Marcelo", 32));
+    static Stream<Pessoa> pessoaStream = listaPessoas.stream();
+
     public static void main(String[] args) {
-        List<String> animal = Arrays.asList("cachorro", "gato", "cavalo");
-        System.out.println(animal.stream().count());
+        long contaPessoas = contaPessoas();
+        System.out.println(contaPessoas);
+
+
+    }
+
+    private static long contaPessoas() {
+        return pessoaStream.count();
     }
 }
